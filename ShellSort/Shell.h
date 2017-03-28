01c476ca490481d20adfedc6c6ba6e03c607327f
Dr Von Martinez <DrVonMartinez@users.noreply.github.com>
2017-03-27 20:32:25 -0500

    Create Shell.h

---
+++ ShellSort/Shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include<iostream>
+#include<string>
+#include<cmath>
+#include <cstdlib>
+#include"InsertionSort.cpp"
+using namespace std;
+
+
+namespace ShellSort{
+  class Shell
+  {
+  public:
+    static LinkedListNS::LinkedList* sort(LinkedListNS::LinkedList *inputList);
+  private:
+    static LinkedListNS::LinkedList* insertionSort(LinkedListNS::LinkedList *listToSort, int sorterSize);
+  };
+}
+#endif
