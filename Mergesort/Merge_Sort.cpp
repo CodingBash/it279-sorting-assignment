750028307f03a4a72f6072cc04e75d2853b9efde
CodingBash <bbecerr@outlook.com>
2017-03-28 08:11:02 -0500

    Return on bad input

--- Mergesort/Merge_Sort.cpp
+++ Mergesort/Merge_Sort.cpp
@@ -13,11 +13,12 @@ int main()
 {
 	MergeSortNS::LinkedList* list = MergeSortNS::Input().retrieveData();
 	
-	//use merge sort technique to sort the newly created linked list.
-	list->mergeSort(&list->a);
-	
-	MergeSortNS::Output().outputData(list);
-	
+	if (list != NULL) {
+		//use merge sort technique to sort the newly created linked list.
+		list->mergeSort(&list->a);
+
+		MergeSortNS::Output().outputData(list);
+	}
 	return 0;
 }
 
