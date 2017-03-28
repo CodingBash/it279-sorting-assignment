dd0715a97692a5e75e7eb7fd20472adb4bd45153
Dr Von Martinez <DrVonMartinez@users.noreply.github.com>
2017-03-28 03:06:27 -0500

    Finalized Shell.cpp

--- ShellSort/Shell.cpp
+++ ShellSort/Shell.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 LinkedListNS::LinkedList* ShellSort::Shell::sort(LinkedListNS::LinkedList*inputList)
 {
+  cout<<"Sorting...";
   LinkedListNS::LinkedList* listToSort=new LinkedListNS::LinkedList();
   int largestGapSequence=0;
   for(int i=0; i<inputList->size(); i++)
@@ -22,34 +23,58 @@ LinkedListNS::LinkedList* ShellSort::Shell::sort(LinkedListNS::LinkedList*inputL
   //if largestGapSequence equals Zero, then list is empty
   for(int i= largestGapSequence; i>0;i--)
     {
-      listToSort=insertionSort(listToSort,(int)pow(2,i)-1);
+      listToSort=insertionSort(listToSort,pow(2,i)-1);
     }
+  cout<<"Complete\n";
   return listToSort;
 }
 
 
 LinkedListNS::LinkedList* ShellSort::Shell::insertionSort(LinkedListNS::LinkedList *listToSort,int sortSize)
 {
-  for(int i=0; i<listToSort->size()+sortSize;)
+  LinkedListNS::LinkedList* sortedList= new LinkedListNS::LinkedList();
+  for(int i=0; i<listToSort->size() + sortSize;)
     {
-      LinkedListNS::LinkedList temp;
-      for(int j=i; j<sortSize;j++)
-	{
-	  if((j+i)<temp.size())
-	    temp.push_back(listToSort->get(j+i));
-	  else
-	    break;
-	}
-      temp = *Sort::InsertionSort::sort(temp);
-      for(int j=0; j<temp.size(); j++)
-	{
-	  if((j+i)<temp.size())
-	    listToSort->insert(j+i,temp.get(j));
-	  else
-	    break;
-	}
-      temp.clear();
+      LinkedListNS::LinkedList* temp = subList(listToSort,sortSize,i);
+      sortedList->merge(temp);
       i+=sortSize;
     }  
+  for(int i=0;i<listToSort->size()+sortSize;)
+    {
+      sortedList=columnSort(sortedList,sortSize,i);
+      i+=sortSize;
+    }
+
+  return sortedList;
+}
+
+
+LinkedListNS::LinkedList* ShellSort::Shell::subList(LinkedListNS::LinkedList* listToSort, int sortSize, int start) 
+{
+  LinkedListNS::LinkedList* temp = new LinkedListNS::LinkedList();
+  for(int i=0; i<sortSize; i++)
+    {
+      if(start<listToSort->size())
+	temp->push_back(listToSort->get(start));
+      start++;
+    }
+  return temp;
+}
+
+LinkedListNS::LinkedList* ShellSort::Shell::columnSort(LinkedListNS::LinkedList* listToSort, int sortSize, int start)
+{
+  LinkedListNS::LinkedList* temp = new LinkedListNS::LinkedList();
+  for(int i=start; i<listToSort->size();)
+    {     
+      temp->push_back(listToSort->get(i));
+      i+=sortSize;
+    }
+  temp = Sort::InsertionSort::sort(*temp);
+  for(int i=start, count =0; i<listToSort->size();count++)
+    {
+      listToSort->remove(i);
+      listToSort->insert(i,temp->get(count));
+      i+=sortSize;
+    }
   return listToSort;
 }
