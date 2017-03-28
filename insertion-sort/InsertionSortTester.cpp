131745f7d7f50d722f3a6f2737e768707cbca00c
CodingBash <bbecerr@outlook.com>
2017-03-27 19:04:57 -0500

    Insertion sort now working on test data

--- insertion-sort/InsertionSortTester.cpp
+++ insertion-sort/InsertionSortTester.cpp
@@ -1,21 +1,16 @@
 #include "../adt-tester/LinkedList.h"
 #include "InsertionSort.h"
+#include<sstream>
+#include<fstream>
 #include <iostream>
 
+LinkedListNS::NodeData addStringToLinkedList(std::string);
+LinkedListNS::LinkedList* getData();
+
 int main() {
-	LinkedListNS::LinkedList list = LinkedListNS::LinkedList();
-	auto createData = [](int number) -> LinkedListNS::NodeData {
-		LinkedListNS::NodeData data = LinkedListNS::NodeData();
-		data.number = number;
-		return data;
-	};
-	list.push_back(createData(4));
-	list.push_back(createData(2));
-	list.push_back(createData(8));
-	list.push_back(createData(6));
-	list.push_back(createData(9));
+	LinkedListNS::LinkedList* inputList = getData();
 	
-	LinkedListNS::LinkedList* sortedList = Sort::InsertionSort::sort(list);
+	LinkedListNS::LinkedList* sortedList = Sort::InsertionSort::sort(*inputList);
 
 	for (int i = 0; i < sortedList->size(); i++) {
 		std::cout << sortedList->get(i).number << std::endl;
@@ -23,4 +18,46 @@ int main() {
 
 	std::system("PAUSE");
 
+}
+
+LinkedListNS::LinkedList* getData() {
+	LinkedListNS::LinkedList* inputList = new LinkedListNS::LinkedList();
+
+	std::string sequence;
+	std::string filename = "C:\\Users\\bbece\\Documents\\Git-Projects\\Git-School-Projects\\IT279\\sorting-assignment\\insertion-sort\\testingData1.txt";
+	std::ifstream dataInputForSorting(filename);
+	if (dataInputForSorting.is_open())
+	{
+		while (!dataInputForSorting.eof())
+		{
+			std::getline(dataInputForSorting, sequence);
+			inputList->push_back(addStringToLinkedList(sequence));
+		}
+	}
+	else
+	{
+		std::cout << "Unable to open \"" << filename << "\"" << std::endl;
+	}
+	return inputList;
+}
+
+/**
+*Converts a string input into a long and adds it to the LinkedList named dataList
+*
+*/
+LinkedListNS::NodeData addStringToLinkedList(std::string sequence)
+{
+	int storable = 0;
+	if (sequence.size() != 0)
+	{
+		std::stringstream convert(sequence);
+		if (!(convert >> storable));
+		else
+		{
+			LinkedListNS::NodeData temp;
+			temp.number = storable;
+			//std::cout << temp.number << std::endl;
+			return temp;
+		}
+	}
 }
